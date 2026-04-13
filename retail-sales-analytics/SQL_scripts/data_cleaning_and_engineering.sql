e01e782adaadf49bfb4586c43495e1b40b03eeda
Abishek pranav <165824454+AbishekPranavGV@users.noreply.github.com>
2026-04-13 22:01:30 +0530

    Add files via upload

---
+++ retail-sales-analytics/SQL_scripts/data_cleaning_and_engineering.sql
@@ -0,0 +1,6 @@
+﻿Msg 208, Level 16, State 1, Procedure v_RetailSales_Cleaned, Line 12 [Batch Start Line 9]
+Invalid object name 'YOUR_ACTUAL_TABLE_NAME'.
+Msg 208, Level 16, State 1, Line 27
+Invalid object name 'v_RetailSales_Cleaned'.
+
+Completion time: 2026-04-13T21:56:53.7343466+05:30
